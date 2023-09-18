12979565cd5dc2362c771b824280361bcf0fcc0b
Allen Wasupan <allen.wasupan@gmail.com>
2023-09-18 10:54:28 -0400

    10/20 Test Cases

--- edu/ufl/cise/cop4020fa23/Lexer.java
+++ edu/ufl/cise/cop4020fa23/Lexer.java
@@ -12,6 +12,7 @@ package edu.ufl.cise.cop4020fa23;
 import static edu.ufl.cise.cop4020fa23.Kind.EOF;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
 
@@ -19,26 +20,30 @@ import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
 public class Lexer implements ILexer {
 
 	String input;
-
+	int pos;
+	int length = 1;
+	int line = 1;
+	int column = 0;
+    int startPos;
+    char[] source;
 
 	private enum States {
-        START, HAVE_EQUALS, IN_NUMB, IN_IDENT, HAVE_ZERO,HAVE_DOT,IN_FLOAT,STRING_LIT,LEFT,RIGHT,EXC,MINUS,INT_LIT
+        START, HAVE_EQUALS, IN_NUMB, IN_IDENT, HAVE_ZERO,HAVE_DOT,IN_FLOAT,STRING_LIT,LEFT,RIGHT,EXC,MINUS,NUM_LIT, BOX, AND, OR, TIMES, HASH
     }
 
 	public Lexer(String input) {
 		this.input = input;
 	}
-/* 
-	Kind isWord(ArrayList input) {
+ 
+	Kind isWord(char[] source2) {
         // forms arraylist into string
         String str = "";
-        for (Object x : input) {
+        for (Object x : source2) {
             str += x;
         }
-
-        // checks for different Kinds
+        length = pos-startPos;
         return switch (str) {
-            case "true", "false" -> Kind.BOOLEAN_LIT;
+            case "true", "false", "TRUE", "FALSE", "True", "False" -> Kind.BOOLEAN_LIT;
             case "BLACK", "BLUE", "CYAN", "DARK_GRAY", "GRAY", "GREEN", "LIGHT_GRAY", "MAGENTA", "ORANGE", "PINK", "RED", "WHITE", "YELLOW" -> Kind.COLOR_CONST;
             case "if" -> Kind.KW_IF;
             case "fi" -> Kind.KW_FI;
@@ -51,9 +56,8 @@ public class Lexer implements ILexer {
             case "getWidth", "getHeight" -> Kind.IMAGE_OP;
             default -> Kind.IDENT;
         };
-
     }
-
+	
 	int[] getASCII(String s) {
         int[] ascii = new int[s.length()];
         for (int i = 0; i != s.length(); i++) {
@@ -61,18 +65,344 @@ public class Lexer implements ILexer {
         }
         return (ascii);
     }
-	*/
+	
 	Kind L(String input) throws LexicalException {
+        
 		States state = States.START;
-		int pos = 0;
-		int length = 1;
-		int line = 0;
-		int column = 0;
-        char[] chars = (input+'~').toCharArray();
+        startPos = pos;
+		//line = pos;
+	    char[] chars = (input+'~').toCharArray();
+        Kind k;
+        source = new char[0];
 		while(true) {
+            
 			char ch = chars[pos];
-			switch (ch) {
+
+            if (ch != ' ' && ch != '\n') {
+                source = Arrays.copyOf(source, source.length + 1);
+                source[source.length - 1] = ch;
+            }
+            
+			switch (state) {
+                
+				case START -> {
+                    System.out.println("Character: \"" + ch + "\"");
+                    System.out.print("Source: ");
+                    System.out.println(source);
+                    System.out.println("Pos: " + pos);
+                    System.out.println("Length: " + length);
+                    System.out.println("Source Length: " + source.length);
+                    System.out.println("StartPos: " + startPos);
+                    System.out.println("\n");
+                    //startPos=pos;
+                    //column++;
+
+                    if (pos < input.length() && (getASCII(input)[pos] == 10)) {
+                        ch = '\n';
+                    }
+                    switch (ch) {
+                        
+                        case ' ','\t','\r' -> {
+                            pos++;
+                            startPos++;
+                        }
+
+                        case '\n'-> {line++;pos++;startPos=0;}
+
+                        case '+' -> {line=pos;length=1;pos++;return Kind.PLUS;}
+                        case '#' -> {pos++;state = States.HASH;}
+
+                        case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                '$','_' -> {
+                            state = States.IN_IDENT;
+                            pos++;
+                        }
+                        case '\"' -> {
+                            state = States.STRING_LIT;
+                            pos++;length++;
+                        }
+                        case '1','2','3','4','5','6','7','8','9' -> {state = States.NUM_LIT;pos++;}
+                        case '(' -> {pos++;return Kind.LPAREN;}
+                        case ')' -> {pos++;return Kind.RPAREN;}
+                        case '[' -> {pos++;state = States.BOX;}
+                        case ']' -> {pos++;return Kind.RSQUARE;}
+                        case '<' -> {pos++;state = States.LEFT;}
+                        case '>' -> {pos++;state = States.RIGHT;}
+                        case '/' -> {pos++;return Kind.DIV;}
+                        case '*' -> {pos++;state = States.TIMES;}
+                        case '-' -> {state = States.MINUS;pos++;}
+                        case '=' -> {state = States.HAVE_EQUALS; pos++;}
+                        case '0' -> {state = States.HAVE_ZERO;pos++;}
+                        case '%' -> {pos++;return Kind.MOD;}
+                        case '&' -> {state = States.AND;pos++;}
+                        case '|' -> {pos++;state = States.OR;}
+                        //case '@' -> {state = States.STRING_LIT;pos++;}
+                        case '!' -> {state = States.EXC; pos++;}
+                        case ';' -> {pos++;return Kind.SEMI;}
+                        case ',' -> {pos++;column++;return Kind.COMMA;}
+                        case '^' -> {pos++;return Kind.RETURN;}
+                        case '?' -> {pos++;return Kind.QUESTION;}
+                        case '~' -> {return Kind.EOF;}
+                        default -> {throw new LexicalException("lexer bug");}
+                    
+                    }
+                    }
+                case HASH -> {
+                    switch(ch) {
+                        case'#' ->{
+                            while(ch != '\n') {
+                                    pos++;
+                                    ch = chars[pos];
+                                    source[0] = ch;
+                                    startPos=pos;
+                                    
+                                }
+                        }default -> throw new LexicalException("lexer bug");
+                                
+                            }
+                            
+                        }
+                
+                case TIMES -> {
+                    switch(ch){
+                                case'*'-> {
+                                source = Arrays.copyOf(source, source.length + 1);
+                                source[source.length - 1] = ch;
+                                pos++;
+                                length=pos-startPos;
+                                return Kind.EXP;
+                            }
+                            default -> {return Kind.TIMES;}
+                            }
+                }
+                case OR -> {
+                    switch(ch) {
+                        case '|' -> {
+                        source = Arrays.copyOf(source, source.length + 1);
+                        source[source.length - 1] = ch;
+                        pos++;
+                        length=pos-startPos;
+                        return Kind.OR;
+                        }
+                    default -> {
+                        return Kind.BITOR;
+                    }
+                    }
+                }
+                case AND -> {
+                    switch (ch) {
+                        case '&' -> {
+                            source = Arrays.copyOf(source, source.length + 1);
+                            source[source.length - 1] = ch;
+                            pos++;
+                            length=pos-startPos;
+                            return Kind.AND;
+                        }
+                        default -> {
+                            return Kind.BITAND;
+                        }
+                    }
+                }
+                case BOX -> {
+                    switch (ch) {
+                        case ']' -> {
+                            pos++;
+                            return Kind.BOX;
+                        }
+                        default -> {
+                            return Kind.LSQUARE;
+                        }
+                    }
+                }
+
+				case IN_IDENT-> {
+                    
+                    switch(ch) {
+                        case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                '$','_','0','1','2','3','4','5','6','7','8','9' -> {
+                            
+                            pos++;
+                            //source = Arrays.copyOf(source, source.length + 1);
+                            //source[source.length - 1] = ch;
+                            
+                            System.out.println("Source ");
+                            System.out.println(source);
+                        }
+                        
+                        default -> {
+                            return isWord(source);
+                        }
+                    }
+                }
+				case HAVE_ZERO -> {
+                    switch(ch) {
+                        case '.' -> {
+                            source = Arrays.copyOf(source, source.length + 1);
+                            source[source.length - 1] = ch;
+                            state = States.HAVE_DOT;
+                            pos++;
+                        }
+                        default -> {
+                            return Kind.NUM_LIT;
+                        }
+                    }
+                }
+                
 				
+				case HAVE_DOT -> {
+                    switch(ch) {
+                        case '0','1','2','3','4','5','6','7','8','9' -> {
+                            state = States.IN_FLOAT;
+                            pos++;
+                        }
+                        default -> throw new LexicalException("lexer bug");
+
+                    }
+                }
+                case HAVE_EQUALS -> {
+                    switch (ch) {
+                        case '=' -> {
+                            pos++;
+                            length++;
+                            return Kind.EQ;
+                        }
+                        default -> {return Kind.ASSIGN;}
+                        
+                    }
+                }
+
+                case IN_NUMB -> {
+                    switch(ch) {
+                        case '0','1','2','3','4','5','6','7','8','9' -> {
+                            
+                            source = Arrays.copyOf(source, source.length + 1);
+                            source[source.length - 1] = ch;
+                            pos++;
+                        }
+                        default -> {
+                            length = pos-startPos;
+                            return Kind.NUM_LIT;
+                        }
+                    }
+                }
+
+				
+                case NUM_LIT -> {
+                    
+                    switch(ch) {
+                        case '0','1','2','3','4','5','6','7','8','9' -> {
+                            
+                            source = Arrays.copyOf(source, source.length + 1);
+                            source[source.length - 1] = ch;
+                            if (pos-startPos > 11) {
+                                throw new LexicalException("lexer bug");
+                            }
+                            pos++;
+                            length=pos-startPos;
+                        }
+                        default -> {
+                            return Kind.NUM_LIT;
+
+                        }
+                    }
+                }
+                case STRING_LIT -> {
+                    
+                    switch(ch) {
+                        case '"' -> {
+                            
+                            pos++;
+                            return Kind.STRING_LIT;
+                        }
+                        default -> {
+                            //source = Arrays.copyOf(source, source.length + 1);
+                            //source[source.length - 1] = ch;
+                            pos++;
+                            length++;
+                        }
+                    }
+                }
+                case MINUS -> {
+                    switch (ch) {
+                        case '>' -> {
+                            pos++;
+                            return Kind.RARROW;
+                        }
+                        default -> {
+                            //pos++;
+                            return Kind.MINUS;
+                        }
+                    }
+                }
+                case EXC -> {
+                    switch (ch) {
+                        case '=' -> {
+                            pos++;
+                            return Kind.NOT_EQUALS;
+                        }
+                        default -> {
+                            //pos++;
+                            return Kind.BANG;
+                        }
+                    }
+                }
+                case LEFT -> {
+                    switch (ch) {
+                        case '=' -> {
+                            pos++;
+                            length++;
+                            return Kind.LE;
+                        }
+                        case '-' -> {
+                            pos++;
+                            length++;
+                            return Kind.LARROW;
+                        }
+                        case ':' -> {
+                            pos++;
+                            length++;
+                            return Kind.BLOCK_OPEN;
+                        }
+                        default -> {
+                            //pos++;
+                            return Kind.LT;
+                        }
+                    }
+
+                }
+                case RIGHT -> {
+                    switch (ch) {
+                        case '=' -> {
+                            pos++;
+                            length++;
+                            return Kind.GE;
+                        }
+                        default -> {
+                            //pos++;
+                            return Kind.GT;
+                        }
+                    }
+                }
+                
+                
+                // if character is an identifier
+                
+                case IN_FLOAT -> {
+					switch(ch) {
+                        case '0','1','2','3','4','5','6','7','8','9' -> {
+                            source = Arrays.copyOf(source, source.length + 1);
+                            source[source.length - 1] = ch;
+                            pos++;
+                        }
+                        default -> {
+                            return Kind.NUM_LIT;
+                        }
+                    }
+                }
+				
+				//default -> {return Kind.EOF;}
 				default -> throw new LexicalException("lexer bug");
 			}
 		}
@@ -80,10 +410,8 @@ public class Lexer implements ILexer {
 	}
 	@Override
 	public IToken next() throws LexicalException {
-		return new Token(L(input), 0, 0, null, new SourceLocation(1, 1));
+
+		return new Token(L(input), startPos, length, source, new SourceLocation(line, column));
 	}
-
-
-
-
 }
+
