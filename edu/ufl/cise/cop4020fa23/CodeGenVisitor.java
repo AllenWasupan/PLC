16a6220e0d01604a37ec1368df562b615e5d62c8
Allen Wasupan <allen.wasupan@gmail.com>
2023-11-25 17:12:55 -0500

    start

--- edu/ufl/cise/cop4020fa23/CodeGenVisitor.java
+++ edu/ufl/cise/cop4020fa23/CodeGenVisitor.java
@@ -3,6 +3,7 @@ package edu.ufl.cise.cop4020fa23;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
+import java.awt.Color;
 
 import edu.ufl.cise.cop4020fa23.ast.ASTVisitor;
 import edu.ufl.cise.cop4020fa23.ast.AssignmentStatement;
@@ -45,17 +46,30 @@ public class CodeGenVisitor implements ASTVisitor {
 		System.out.println("CodeGenVisitor");
         map = new HashMap<>();
 	}
-
+    String type(String t) {
+        t = t.toLowerCase();
+        if (t.equals("string")) {
+            t = "String";
+        }
+        if (t.equals("image")) {
+            t = "BufferedImage";
+        }
+        if (t.equals("pixel")) {
+            t = "int";
+        }
+        if (t.equals("boolean")) {
+            t = "Boolean";
+        }
+        return t;
+    }
     @Override
     public Object visitProgram(Program program, Object arg) throws PLCCompilerException {
         System.out.println("visitProgramGen");
         String s;
         String p;
         String t = program.getType().name();
-        t = t.toLowerCase();
-        if (t.charAt(0) == 's') {
-            t = 'S' + t.substring(1);
-        }
+        t = type(t);
+
         String b = (String) program.getBlock().visit(this, arg);
 
         p = "";
@@ -65,12 +79,7 @@ public class CodeGenVisitor implements ASTVisitor {
                 p += ",";
             }
             para = program.getType().toString().toLowerCase() + " " + (String) program.getParams().get(i).visit(this, arg);
-            if (para.charAt(0) == 's') {
-                para = 'S' + para.substring(1);
-            }
-            if (para.charAt(0) == 's') {
-                para = 'S' + para.substring(1);
-            }
+            para = type(para);
             
             p +=  para;
             
@@ -100,6 +109,7 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitBlock(Block block, Object arg) throws PLCCompilerException {
+        System.out.println("visitBlockGen");
         String s;
         String e = "";
         List<BlockElem> elems = block.getElems();
@@ -113,13 +123,15 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitNameDef(NameDef nameDef, Object arg) throws PLCCompilerException {
-        System.out.println("visitNameDefG");
+        System.out.println("visitNameDefGen");
         
         String t = nameDef.getType().name();
-        t = t.toLowerCase();
+        t = type(t);
         String s;
         s =  nameDef.getIdentToken().text();
-
+        
+        System.out.println("t " + t);
+        System.out.println("s " + s);
         String n;
         if (map.get(s) == null) {
             Stack<String> narr;
@@ -266,6 +278,7 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitLValue(LValue lValue, Object arg) throws PLCCompilerException {
+        System.out.println("visitLValueGen");
         String s;
         s = (String) lValue.getNameDef().visit(this, arg);
         return s;
@@ -273,6 +286,7 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitAssignmentStatement(AssignmentStatement assignmentStatement, Object arg) throws PLCCompilerException {
+        System.out.println("visitAssignmentStatementGen");
         String s;
         String l = (String) assignmentStatement.getlValue().visit(this, arg);
         String e = (String) assignmentStatement.getE().visit(this, arg);
@@ -302,6 +316,7 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitBlockStatement(StatementBlock statementBlock, Object arg) throws PLCCompilerException {
+        System.out.println("visitBlockStatementGen");
         String s;
         s = (String) statementBlock.getBlock().visit(this, arg) + ";";
         return s;
@@ -309,65 +324,132 @@ public class CodeGenVisitor implements ASTVisitor {
 
     @Override
     public Object visitBooleanLitExpr(BooleanLitExpr booleanLitExpr, Object arg) throws PLCCompilerException {
+        System.out.println("visitBooleanLitExprGen");
         String s;
         s = (String) booleanLitExpr.getText();
         return s;
     }
 
-
-
     //Ignore until assignment 5
 
     @Override
     public Object visitConstExpr(ConstExpr constExpr, Object arg) throws PLCCompilerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitConstExpr'");
+        System.out.println("visitConstExprGen");
+        String n = constExpr.getName();
+        System.out.println("n" + n + ".");
+        int c;
+        if (n.equals("Z")) {
+            return "255";
+        }
+        else {
+            
+            switch (n.toLowerCase()) {
+                case "red":
+                    c = Color.RED.getRGB();
+                    break;
+                case "blue":
+                    c = Color.BLUE.getRGB();
+                    break;
+                case "green":
+                    c = Color.GREEN.getRGB();
+                    break;
+                case "gray":
+                    c = Color.GRAY.getRGB();
+                    break;
+                case "pink":
+                    c = Color.PINK.getRGB();
+                    break;
+                case "black":
+                    c = Color.BLACK.getRGB();
+                    break;
+                case "cyan":
+                    c = Color.CYAN.getRGB();
+                    break;
+                case "dark_gray":
+                    c = Color.DARK_GRAY.getRGB();
+                    break;
+                case "light_gray":
+                    c = Color.lightGray.getRGB();
+                    break;
+                case "magenta":
+                    c = Color.MAGENTA.getRGB();
+                    break;
+                case "white":
+                    c = Color.WHITE.getRGB();
+                    break;
+                default:
+                    throw new CodeGenException();
+            }
+            
+            
+            return "0x" +
+            Integer.toHexString(c);
+        }
     }
 
     @Override
     public Object visitPostfixExpr(PostfixExpr postfixExpr, Object arg) throws PLCCompilerException {
+    System.out.println("visitPostfixExprGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitPostfixExpr'");
     }
 
     @Override
     public Object visitChannelSelector(ChannelSelector channelSelector, Object arg) throws PLCCompilerException {
+    System.out.println("visitChannelSelectorGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitChannelSelector'");
     }
     
     @Override
     public Object visitPixelSelector(PixelSelector pixelSelector, Object arg) throws PLCCompilerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitPixelSelector'");
+    System.out.println("visitPixelSelectorGen");
+        String s = "";
+        s += pixelSelector.xExpr().visit(this, arg);
+        s += ",";
+        s += pixelSelector.yExpr().visit(this, arg);
+        return s;
     }
 
     @Override
     public Object visitExpandedPixelExpr(ExpandedPixelExpr expandedPixelExpr, Object arg) throws PLCCompilerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitExpandedPixelExpr'");
+        System.out.println("visitExpandedPixelExprGen");
+        String s = "PixelOps.pack(";
+        System.out.println("s " + s);
+        s += expandedPixelExpr.getRed().visit(this, arg).toString();
+        System.out.println("s " + s);
+        s += ",";
+        s += expandedPixelExpr.getGreen().visit(this, arg);
+        s += ",";
+        s += expandedPixelExpr.getBlue().visit(this, arg);
+        s += ")";
+        return s;
     }
 
     @Override
     public Object visitDimension(Dimension dimension, Object arg) throws PLCCompilerException {
+    System.out.println("visitDimensionGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitDimension'");
     }
 
     @Override
     public Object visitDoStatement(DoStatement doStatement, Object arg) throws PLCCompilerException {
+    System.out.println("visitDoStatementGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitDoStatement'");
     }
 
     @Override
     public Object visitIfStatement(IfStatement ifStatement, Object arg) throws PLCCompilerException {
+    System.out.println("visitIfStatementGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitIfStatement'");
     }
 
     @Override
     public Object visitGuardedBlock(GuardedBlock guardedBlock, Object arg) throws PLCCompilerException {
+    System.out.println("visitGuardedBlockGen");
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visitGuardedBlock'");
     }
